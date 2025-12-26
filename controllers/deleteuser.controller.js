e6a684dec03f40ba173c434efb12eb51d02b9d2b
VirajxD0 <virajvd99@gmail.com>
2025-12-26 23:57:07 +0530

    final commit

---
+++ controllers/deleteuser.controller.js
@@ -0,0 +1,9 @@
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    next(error);
+  }
+};
